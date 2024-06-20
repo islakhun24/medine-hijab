77646cb1604d58c13d0bfbcc00be8b24eeba48d0
Imam Diki Ridmawan <imamdikiridmawan@gmail.com>
2024-06-20 11:45:04 +0700

    change layout condition

--- pages/form_page/form_page.vue
+++ pages/form_page/form_page.vue
@@ -9,7 +9,7 @@
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLabel"><i>Contact Us</i></h5>
           <button
             type="button"
             class="close"
@@ -26,35 +26,44 @@
               v-model="inputName"
               type="text"
               class="form-control fadeIn first"
-              placeholder="nathan joe"
+              placeholder="input your name"
             />
           </div>
           <div class="mb-3">
-            <label class="form-label fadeIn second">Email</label>
-            <input
-              v-model="inputEmail"
-              type="email"
-              class="form-control fadeIn second"
-              placeholder="jhon@gmail.com"
-              @blur="validateEmail"
-            />
-            <br />
-            <small v-if="emailError" class="text-danger">{{
-              emailError
-            }}</small>
+            <label class="form-label fadeIn second">Phone Number</label>
+            <div class="input-group fadeIn second">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">+62</span>
+              </div>
+              <input
+                type="text"
+                style="border-left: none; padding-left: 0"
+                class="form-control"
+                v-model="inputPhone"
+                placeholder="input your phone number"
+                pattern="^[1-9][0-9]*$"
+                maxlength="15"
+                oninput="if(!this.value.match('^[8][0-9]*$'))this.value=''"
+              />
+            </div>
+            <small class="text-danger fadeIn second"
+              >*input starts from number 8</small
+            >
           </div>
         </div>
         <div class="modal-footer">
           <button
             type="button"
-            :disabled="isDisabled"
+            :disabled="inputName === '' || inputPhone === ''"
             :class="
-              isDisabled ? 'btn btn-secondary w-50' : 'btn btn-primary w-50'
+              inputName === '' || inputPhone === ''
+                ? 'btn btn-secondary w-50'
+                : 'btn btn-primary w-50'
             "
             data-dismiss="modal"
             @click="sendData"
           >
-            Confirm
+            <i class="text-btn">Send</i>
           </button>
         </div>
       </div>
@@ -63,45 +72,38 @@
 </template>
 
 <script>
-import debounce from 'lodash/debounce'
-
 export default {
   name: 'modalForm',
   data() {
     return {
       inputName: '',
-      inputEmail: '',
-      emailError: '',
-      isDisabled: true,
+      inputPhone: '',
     }
   },
   methods: {
     sendData() {
-      console.log(this.inputName, 'ini input nama')
-      console.log(this.inputEmail, 'ini input email')
-    },
-    validateEmail: debounce(function () {
-      if (!this.inputEmail) {
-        this.isDisabled = true
-        this.emailError = 'Email wajib diisi'
-        return
-      }
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-      if (!emailRegex.test(this.inputEmail)) {
-        this.isDisabled = true
-        this.emailError = 'Format email tidak valid'
-      } else {
-        this.isDisabled = false
-        this.emailError = ''
-      }
-    }, 1000),
-  },
-  watch: {
-    inputEmail() {
-      this.validateEmail()
+      const phoneNumber = '6285163523959'
+      const message = `*${this.inputName}* and *${'0' + this.inputPhone}*`
+      const url = `https://wa.me/${phoneNumber}?text=${message}`
+      window.open(url, '_blank')
     },
   },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.input-group-text {
+  background: transparent;
+  border-right: none;
+  border-radius: 20px;
+}
+.modal-title {
+  font-family: 'Poppins';
+  font-weight: 500;
+  font-size: 24px;
+}
+.text-btn {
+  font-family: 'Poppins';
+  font-weight: 500;
+}
+</style>
